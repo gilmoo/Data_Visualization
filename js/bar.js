0c2116a92d59cd0f8dc37814a72778065aa2411e
Ruthger van den Eikhof <ruthger.vandeikhof@student.fontys.nl>
2018-01-08 22:41:48 +0100

    test

--- js/bar.js
+++ js/bar.js
@@ -30,12 +30,12 @@ d3.csv("csv/belgie.csv", function (data) {
 d3.csv("csv/belgie_thuis.csv", function (data) {
     //barchart
     console.log(data[0]);
-    var svg3 = dimple.newSvg("#chartContainer3", 690, 400);
-    var myChart2 = new dimple.chart(svg3, data);
-    myChart2.setBounds(200, 30, 480, 330);
-    myChart2.addMeasureAxis("x", "Home game points");
-    var y = myChart2.addCategoryAxis("y", "Team name");
+    var svg4 = dimple.newSvg("#chartContainer3", 690, 400);
+    var myChart3 = new dimple.chart(svg4, data);
+    myChart3.setBounds(200, 30, 480, 330);
+    myChart3.addMeasureAxis("x", "Home game points");
+    var y = myChart3.addCategoryAxis("y", "Team name");
     //y.addOrderRule("Date");
-    myChart2.addSeries(null, dimple.plot.bar);
-    myChart2.draw();
+    myChart3.addSeries(null, dimple.plot.bar);
+    myChart3.draw();
   });
