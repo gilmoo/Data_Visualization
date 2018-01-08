96912034f8af92d3bb68df35a91f743c3221cd53
Ruthger van den Eikhof <ruthger.vandeikhof@student.fontys.nl>
2018-01-08 19:46:13 +0100

    test

--- js/stream_layers.js
+++ js/stream_layers.js
@@ -2,8 +2,8 @@
 function stream_layers(n, m, o) {
     if (arguments.length < 3) o = 0;
     function bump(a) {
-      var x = 1 / (.1 + Math.random()),
-          y = 'test',
+      var x = 'test',
+          y = 2 * Math.random() - .5,
           z = 10 / (.1 + Math.random());
       for (var i = 0; i < m; i++) {
         var w = (i / m - y) * z;
