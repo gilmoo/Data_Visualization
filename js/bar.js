455f12bc6fbce4efecddddb2351d98d18afe2122
Ruthger van den Eikhof <ruthger.vandeikhof@student.fontys.nl>
2018-01-08 21:44:45 +0100

    test

--- js/bar.js
+++ js/bar.js
@@ -16,7 +16,7 @@ var svg2 = d3.select("#chartContainer")
 // var svg = dimple.newSvg("#chartContainer", 590, 400);
 d3.csv("csv/belgie.csv", function (data) {
   //barchart
-  console.log(data);
+  console.log(data[0]);
   var svg3 = dimple.newSvg("#chartContainer2", 690, 400);
   var myChart2 = new dimple.chart(svg3, data);
   myChart2.setBounds(200, 30, 480, 330);
