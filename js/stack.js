fd3a67603226e316ff08229a29f63aeb44d8095f
Ruthger van den Eikhof <ruthger.vandeikhof@student.fontys.nl>
2018-01-08 20:30:38 +0100

    test

--- js/stack.js
+++ js/stack.js
@@ -1,151 +1,151 @@
 
 
 var n = 3, // number of layers
-m = 18, // number of samples per layer
-data = d3.layout.stack()(stream_layers(n, m, .1)),
-color = d3.interpolateRgb("#aad", "#556"),
-label = ['Bayern München','Borussia Dortmund','Bayer Leverkusen','Borussia Mönchengladbach','Schalke 04','Mainz','Hertha BSC',
-'Wolfsburg','Köln','Hamburg','Ingolstadt','Augsburg','Werder Bremen','Darmstadt','Hoffenheim','Eintracht Frankfurt','Stuttgart','Hannover 96'];
+    m = 18, // number of samples per layer
+    data = d3.layout.stack()(stream_layers(n, m, .1)),
+    color = d3.interpolateRgb("#aad", "#556"),
+    label = ['Bayern München', 'Borussia Dortmund', 'Bayer Leverkusen', 'Borussia Mönchengladbach', 'Schalke 04', 'Mainz', 'Hertha BSC',
+        'Wolfsburg', 'Köln', 'Hamburg', 'Ingolstadt', 'Augsburg', 'Werder Bremen', 'Darmstadt', 'Hoffenheim', 'Eintracht Frankfurt', 'Stuttgart', 'Hannover 96'];
+
+var legendRectSize = 18;
+var legendSpacing = 4;
 
 var p = 20,
-w = 960,
-h = 500 - .5 - p,
-mx = m,
-my = d3.max(data, function(d) {
-  return d3.max(d, function(d) {
-    return d.y0 + d.y;
-  });
-}),
-mz = d3.max(data, function(d) {
-  return d3.max(d, function(d) {
-    return d.y;
-  });
-}),
-x = function(d) { return d.x * w / mx; },
-y0 = function(d) { return h - d.y0 * h / my; },
-y1 = function(d) { return h - (d.y + d.y0) * h / my; },
-y2 = function(d) { return d.y * h / mz; }; // or `my` to not rescale
+    w = 960,
+    h = 500 - .5 - p,
+    mx = m,
+    my = d3.max(data, function (d) {
+        return d3.max(d, function (d) {
+            return d.y0 + d.y;
+        });
+    }),
+    mz = d3.max(data, function (d) {
+        return d3.max(d, function (d) {
+            return d.y;
+        });
+    }),
+    x = function (d) { return d.x * w / mx; },
+    y0 = function (d) { return h - d.y0 * h / my; },
+    y1 = function (d) { return h - (d.y + d.y0) * h / my; },
+    y2 = function (d) { return d.y * h / mz; }; // or `my` to not rescale
 
 var vis = d3.select("#chart")
-.append("svg:svg")
-.attr("width", w)
-.attr("height", h + p);
+    .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h + p);
 
 var layers = vis.selectAll("g.layer")
-.data(data)
-.enter().append("svg:g")
-.style("fill", function(d, i) { return color(i / (n - 1)); })
-.attr("class", "layer");
+    .data(data)
+    .enter().append("svg:g")
+    .style("fill", function (d, i) { return color(i / (n - 1)); })
+    .attr("class", "layer");
 
 var bars = layers.selectAll("g.bar")
-.data(function(d) { return d; })
-.enter().append("svg:g")
-.attr("class", "bar")
-.attr("transform", function(d) { return "translate(" + x(d) + ",0)"; });
+    .data(function (d) { return d; })
+    .enter().append("svg:g")
+    .attr("class", "bar")
+    .attr("transform", function (d) { return "translate(" + x(d) + ",0)"; });
 
 bars.append("svg:rect")
-.attr("width", x({x: .9}))
-.attr("x", 0)
-.attr("y", h)
-.attr("height", 0)
-.transition()
-.delay(function(d, i) { return i * 10; })
-.attr("y", y1)
-.attr("height", function(d) { return y0(d) - y1(d); });
+    .attr("width", x({ x: .9 }))
+    .attr("x", 0)
+    .attr("y", h)
+    .attr("height", 0)
+    .transition()
+    .delay(function (d, i) { return i * 10; })
+    .attr("y", y1)
+    .attr("height", function (d) { return y0(d) - y1(d); });
 
 var labels = vis.selectAll("text.label")
-.data(data[0])
-.enter().append("svg:text")
-.attr("class", "label")
-.attr("x", x)
-.attr("y", h + 6)
-.attr("dx", x({x: .45}))
-.attr("dy", ".71em")
-.attr("text-anchor", "middle")
-.text(function(d, i) { return label[i]; });
+    .data(data[0])
+    .enter().append("svg:text")
+    .attr("class", "label")
+    .attr("x", x)
+    .attr("y", h + 6)
+    .attr("dx", x({ x: .45 }))
+    .attr("dy", ".71em")
+    .attr("text-anchor", "middle")
+    .text(function (d, i) { return label[i]; });
 
 vis.append("svg:line")
-.attr("x1", 0)
-.attr("x2", w - x({x: .1}))
-.attr("y1", h)
-.attr("y2", h);
+    .attr("x1", 0)
+    .attr("x2", w - x({ x: .1 }))
+    .attr("y1", h)
+    .attr("y2", h);
 
-var legend = svg.append("g")
-    .attr("class", "legend")
-    .attr("x", w - 65)
-    .attr("y", 25)
-    .attr("height", 100)
-    .attr("width", 100);
-
-    legend.selectAll('g').data(dataset)
+var legend = svg.selectAll('.legend')
+    .data(color.domain())
     .enter()
     .append('g')
-    .each(function(d, i) {
-      var g = d3.select(this);
-      g.append("rect")
-        .attr("x", w - 65)
-        .attr("y", i*25)
-        .attr("width", 10)
-        .attr("height", 10)
-        .style("fill", color_hash[String(i)][1]);
+    .attr('class', 'legend')
+    .attr('transform', function (d, i) {
+        var height = legendRectSize + legendSpacing;
+        var offset = height * color.domain().length / 2;
+        var horz = -2 * legendRectSize;
+        var vert = i * height - offset;
+        return 'translate(' + horz + ',' + vert + ')';
+    });
 
-      g.append("text")
-        .attr("x", w - 50)
-        .attr("y", i * 25 + 8)
-        .attr("height",30)
-        .attr("width",100)
-        .style("fill", color_hash[String(i)][1])
-        .text(color_hash[String(i)][0]);
-        
+legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+    legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
+
 function transitionGroup() {
-var group = d3.selectAll("#chart");
+    var group = d3.selectAll("#chart");
 
-group.select("#group")
-  .attr("class", "first active");
+    group.select("#group")
+        .attr("class", "first active");
 
-group.select("#stack")
-  .attr("class", "last");
+    group.select("#stack")
+        .attr("class", "last");
 
-group.selectAll("g.layer rect")
-.transition()
-  .duration(500)
-  .delay(function(d, i) { return (i % m) * 10; })
-  .attr("x", function(d, i) { return x({x: .9 * ~~(i / m) / n}); })
-  .attr("width", x({x: .9 / n}))
-  .each("end", transitionEnd);
+    group.selectAll("g.layer rect")
+        .transition()
+        .duration(500)
+        .delay(function (d, i) { return (i % m) * 10; })
+        .attr("x", function (d, i) { return x({ x: .9 * ~~(i / m) / n }); })
+        .attr("width", x({ x: .9 / n }))
+        .each("end", transitionEnd);
 
-function transitionEnd() {
-d3.select(this)
-  .transition()
-    .duration(500)
-    .attr("y", function(d) { return h - y2(d); })
-    .attr("height", y2);
-}
+    function transitionEnd() {
+        d3.select(this)
+            .transition()
+            .duration(500)
+            .attr("y", function (d) { return h - y2(d); })
+            .attr("height", y2);
+    }
 }
 
 function transitionStack() {
-var stack = d3.select("#chart");
+    var stack = d3.select("#chart");
 
-stack.select("#group")
-  .attr("class", "first");
+    stack.select("#group")
+        .attr("class", "first");
 
-stack.select("#stack")
-  .attr("class", "last active");
+    stack.select("#stack")
+        .attr("class", "last active");
 
-stack.selectAll("g.layer rect")
-.transition()
-  .duration(500)
-  .delay(function(d, i) { return (i % m) * 10; })
-  .attr("y", y1)
-  .attr("height", function(d) { return y0(d) - y1(d); })
-  .each("end", transitionEnd);
+    stack.selectAll("g.layer rect")
+        .transition()
+        .duration(500)
+        .delay(function (d, i) { return (i % m) * 10; })
+        .attr("y", y1)
+        .attr("height", function (d) { return y0(d) - y1(d); })
+        .each("end", transitionEnd);
 
-function transitionEnd() {
-d3.select(this)
-  .transition()
-    .duration(500)
-    .attr("x", 0)
-    .attr("width", x({x: .9}));
-}
+    function transitionEnd() {
+        d3.select(this)
+            .transition()
+            .duration(500)
+            .attr("x", 0)
+            .attr("width", x({ x: .9 }));
+    }
 }
 
